8483735a10437b56d765bded94ffff42bedc4749
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-06-23 08:30:53 -0400

    Added text align to newdrink and current drink in style.css. Added name of drink in another-drink-template in index.html. Added setstartover function, another-drink-form submit, getrandomnumber, resetquestionnumber to bartender prototype, getdrinkadjective/noun to pantry, and removed get/setingredientlistnumber from pantry in app.js.

--- js/app.js
+++ js/app.js
@@ -7,43 +7,84 @@ $(document).ready(function() {
      * @param data - data returned from ajax called
      */
     function getDataCallback(data) {
+        //initialize bartender and pantry objects
         Jack = new Bartender(data.questions);
-        TheDrunkenPirate = new Pantry(data.ingredients);
+        TheDrunkenPirate = new Pantry(data.ingredients, data['drink adjective'], data['drink noun']);
+        //set the template to use on the web page and fill with data
         setTemplate();
     }
     /*
-     *
+     * Function to set up the question template, fill it with data, and display it
      */
     function setTemplate() {
+        //data needed to fill template
         var key = Object.keys(Jack.getQuestion(Jack.getQuestionNumber()));
         var currentQuestion = Jack.getQuestion(Jack.getQuestionNumber())[key];
         var types = TheDrunkenPirate.getIngredientType(key);
+        //template to use
         var source = $("#question-template").html();
         var template = Handlebars.compile(source);
-        var context = {
+        var context = { //fields to be filled in the template
             drink_question: currentQuestion,
             answer1: types[0],
             answer2: types[1],
             answer3: types[2]
         };
-        var html = template(context);
-        $('.question-section').html(html);
+        var html = template(context); //set the fields in the template
+        $('.question-section').html(html); //add to the html
     }
     /*
-     * Function to handle submit form
+     * Function to ask the bartender for another drink, deals with showing your drink
+     * and shows a button to ask for another drink
+     */
+    function setStartOver() {
+        //template to use
+        var source = $("#another-drink-template").html();
+        var template = Handlebars.compile(source);
+        //data to fill the template, uses randomnumber function to determine the drink adjective and noun
+        var drinkName = TheDrunkenPirate.getDrinkAdjective(getRandomNumber(10)).concat(' ' + TheDrunkenPirate.getDrinkNoun(getRandomNumber(10)));
+        var context = { //set the fields in the template
+            drink: drinkName
+        };
+        var html = template(context); //set the fields in the template
+        $('.question-section').html(html); //add to the html
+    }
+    /*
+     * Function that handles getting drink information, deals with asking the user drink information
+     * or asking if the user wants another drink
      */
     $('.question-section').on('submit', '#question-form', function(event) {
-        event.preventDefault();
+        event.preventDefault(); //block page reload
+        //if the current question number is less than the total amount of questions
         if (Jack.getQuestionNumber() < (Jack.listOfQuestions.length)) {
-            var genreChecked = $('.answer-choices input:checked').val();
-            Jack.setQuestionNumber();
+            Jack.setQuestionNumber(); //increment question number
+            //if statement to stop if question number is now max
             if (Jack.getQuestionNumber() !== (Jack.listOfQuestions.length)) {
-                setTemplate();
+                setTemplate(); //set the question template
             } else {
-
+                setStartOver(); //show drink and as if you want more to drink
             }
         }
     });
+    /*
+     * Function to ask if you want another drink
+     */
+    $('.question-section').on('submit', '#another-drink-form', function(event) {
+        event.preventDefault(); //prevent page reload
+        if (Jack && TheDrunkenPirate) { //if the objects exist
+            Jack.resetQuestionNumber(); //reset question number to 0
+            setTemplate(); //show the question template
+        } else {
+            //otherwise make ajax call to fill objects
+            getQuestionsIngredients(getDataCallback);
+        }
+    });
+    /*
+    * Random number generator function
+    */
+    function getRandomNumber(number) {
+        return parseInt(Math.random() * (number));
+    }
     /*
      * Questions object to store questions
      * @param questions - list of questions
@@ -81,6 +122,10 @@ $(document).ready(function() {
     Bartender.prototype.setQuestionNumber = function() {
         this.questionNumber++;
     };
+    //Reset question number for another round
+    Bartender.prototype.resetQuestionNumber = function() {
+        this.questionNumber = 0;
+    };
     /*
      * Ingredients object to store list of ingredients
      * @param list - list of ingredients to store
@@ -102,21 +147,21 @@ $(document).ready(function() {
      * Pantry object that wraps ingredients object.
      * @param list of ingredients to initialize ingredients
      */
-    function Pantry(list) {
+    function Pantry(list, drinkAdjective, drinkNoun) {
         Ingredients.call(this, list);
-        this.ingredientListNumber = 0;
+        this.drinkAdjective = drinkAdjective;
+        this.drinkNoun = drinkNoun;
     }
     //subclass extending superclass
     Pantry.prototype = Object.create(Ingredients.prototype);
     Pantry.prototype.contructor = Pantry;
-    /*
-     * Getter and setter functions for ingredient number
-     */
-    Pantry.prototype.getIngredientListNumber = function() {
-        return this.ingredientListNumber;
+    //getter for drink adjective
+    Pantry.prototype.getDrinkAdjective = function(index) {
+        return this.drinkAdjective[index];
     };
-    Pantry.prototype.setIngredientListNumber = function() {
-        this.ingredientListNumber++;
+    //getter for drink noun
+    Pantry.prototype.getDrinkNoun = function(index) {
+        return this.drinkNoun[index];
     };
     /*
      * Function to get the questions and ingredients from json file
